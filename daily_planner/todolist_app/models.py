bf6b6412184899f542d8761295bfff3238e48704
AJ <artjasan@gmail.com>
2021-02-08 01:00:08 +0100

    Creating new models in To-do app, adding basic view for that app

--- daily_planner/todolist_app/models.py
+++ daily_planner/todolist_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    second_name = models.CharField(max_length=50)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Task(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE,)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,)
+    title = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.title
