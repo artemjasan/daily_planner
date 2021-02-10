2c89aaf5a0fe067e1ba13c134a8cd8facd003612
AJ <artjasan@gmail.com>
2021-02-10 21:48:07 +0100

    Update models of Todolist_app

--- daily_planner/todolist_app/models.py
+++ daily_planner/todolist_app/models.py
@@ -1,19 +1,22 @@
 from django.db import models
 from django.utils import timezone
 
-class User(models.Model):
-    first_name = models.CharField(max_length=50)
-    second_name = models.CharField(max_length=50)
-
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+    class Meta:
+        verbose_name = ("Category")
+        verbose_name_plural = ("Categories")
+
+    def _str_(self):
+        return self.name
+
 
 class Task(models.Model):
-    creator = models.ForeignKey(User, on_delete=models.CASCADE,)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE,)
+    category = models.ForeignKey(Category, default=False, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
+    description = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=False)
 
--- daily_planner/todolist_app/views.py
+++ daily_planner/todolist_app/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Task
 
 def index(request):
-    return render(request, 'todolist_app.html')
+    tasks = Task.objects.filter(date_on=timezone.now()).order_by('date')
+
+    if request.method == "POST":
+        new_task = Task(
+            title=request.POST['title'],
+            #category=request.POST['category'],
+        )
+        new_task.save()
+
+    return render(request, 'todolist_app/todolist_app.html', {'tasks':tasks})
