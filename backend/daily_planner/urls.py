fa9204888451914d541ee196ed61bfed50ba2bbe
AJ <artjasan@gmail.com>
2021-02-26 11:14:52 +0100

    Update urls path of project

--- backend/daily_planner/urls.py
+++ backend/daily_planner/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todo_list/', include('todolist_app.urls')),
+    path('api/todo_list/', include('todolist_app.urls')),
 
 ]
