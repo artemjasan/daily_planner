50ceb52a0c035a316b1a62c8a010ee01cf7eccf1
AJ <artjasan@gmail.com>
2021-03-03 22:33:48 +0100

    Update model to intchoices subclasses and add new endpoints, views

--- backend/todolist_app/views.py
+++ backend/todolist_app/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .models import Task
 from .serializers import TasksSeralizer
@@ -12,3 +14,19 @@ class ListTask(generics.ListCreateAPIView):
 class DetailTask(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TasksSeralizer
+
+
+class CategoriesList(APIView):
+    def get(self, request, format=None):
+        categories_dict = {
+            int: str for int, str in Task.TaskCategories.choices
+        }
+        return Response(categories_dict)
+
+
+class PrioritiesList(APIView):
+    def get(self, request, format=None):
+        priority_dict = {
+            int: str for int, str in Task.TaskPriority.choices
+        }
+        return Response(priority_dict)
