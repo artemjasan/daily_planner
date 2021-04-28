871b33d3ba339a0c87069b63cba5a8b03af282eb
AJ <artjasan@gmail.com>
2021-04-28 23:26:58 +0200

    Resolve questions to tasks, hiding sensitive datas by library python-dotenv , update requirements

--- backend/weather_app/views.py
+++ backend/weather_app/views.py
@@ -1,38 +1,62 @@
+import os
 import requests
 
-from rest_framework import generics
-from rest_framework import status
+from dotenv import load_dotenv, find_dotenv
+from rest_framework import generics, status
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
 from .models import City
 from .serializer import CitySerializer
 
-URL_API = "http://api.openweathermap.org/data/2.5/weather?q={}"
-UNIT_SYSTEM = "units=metric"
-API_ID = "appid=74fda670f4fb3091728731208163e6d8"
-API_CALL = f"{URL_API}&{UNIT_SYSTEM}&{API_ID}"
+#  load to the project sensitive dates
+load_dotenv(find_dotenv())
+
+URL_API = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric"
+API_KEY = os.environ.get("API_KEY")
+API_CALL_URL = f"{URL_API}&{API_KEY}"
+MAX_AMOUNT = 3  # Max of cities in list
 
 
-def call_api(city_name):
-    city_weather = requests.get(API_CALL.format(city_name)).json()
-    if city_weather["cod"] == 200:
-        weather_data = {
-            "city": city_name,
-            "temperature": city_weather["main"]["temp"],
-            "description": city_weather["weather"][0]["description"],
-            "icon": city_weather["weather"][0]["icon"]
-        }
-        return weather_data
-    err_msg = {
-        "message": f'City {city_name} does not exist in this world!'
+def parse_weather_data(response):
+    weather_data = response.json()
+    parsed_weather_data = {
+        "temperature": weather_data["main"]["temp"],
+        "description": weather_data["weather"][0]["description"],
+        "icon": weather_data["weather"][0]["icon"]
     }
-    return err_msg
+    return parsed_weather_data
+
+
+def open_weather_api(city_name):
+    data = None
+    error_message = None
+
+    response = requests.get(API_CALL_URL.format(city_name))
+    if response.ok:
+        data = parse_weather_data(response)
+    else:
+        error_message = f'City {city_name} does not exist in this world!'
+
+    return {
+        "city": city_name,
+        "data": data,
+        "error": error_message,
+    }
 
 
 class ListCity(generics.ListCreateAPIView):
     queryset = City.objects.all()
     serializer_class = CitySerializer
+
+    def post(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        if queryset.count() >= MAX_AMOUNT:
+            return Response("Maximum amount of cities", status=status.HTTP_406_NOT_ACCEPTABLE)
+        serializer = CitySerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
 
 
 class DetailCity(generics.RetrieveUpdateDestroyAPIView):
@@ -45,10 +69,5 @@ class CityWeather(GenericAPIView):
 
     def get(self, request):
         cities = self.get_queryset()
-        weather_data = []
-
-        for city in cities:
-            city_weather = call_api(city)
-            weather_data.append(city_weather)
-
+        weather_data = [open_weather_api(city) for city in cities]
         return Response(weather_data)
